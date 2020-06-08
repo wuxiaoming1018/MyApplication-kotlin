85e52d89dc78fd42eab6e909b9ab282682a27105
ming <1398565099@qq.com>
2020-06-08 23:00:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        text.text = "the first application"
+        text.setOnClickListener{
+            Snackbar.make(findViewById(android.R.id.content),"snackbar test",Snackbar.LENGTH_LONG).show()
+        }
+    }
+}
